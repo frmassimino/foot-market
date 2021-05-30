295e1fe0c7e61f0e4765af59f9f9a9de9f58eebc
frmassimino <fr.massimino@gmail.com>
2021-05-30 11:26:55 -0300

    App key file name changed

--- foot_market/settings.py
+++ foot_market/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from typing import Sequence
-from .app_kew import SECRET_KEY
+from .app_key import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
